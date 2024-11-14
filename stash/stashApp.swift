a5a54a62ce15a321cbab3aa6937d0639cd360536
C Krivan <ckrivan@gmail.com>
2024-11-14 12:00:14 -0500

    Initial Commit

---
+++ stash/stashApp.swift
@@ -0,0 +1,17 @@
+//
+//  stashApp.swift
+//  stash
+//
+//  Created by Charles Krivan on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct stashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stashTests/stashTests.swift
@@ -0,0 +1,17 @@
+//
+//  stashTests.swift
+//  stashTests
+//
+//  Created by Charles Krivan on 11/14/24.
+//
+
+import Testing
+@testable import stash
+
+struct stashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
